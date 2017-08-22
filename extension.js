0959d8cbf1d2b314dbb3ff9d4c3260b7fd33e9aa
Martin Schumacher <schumarty@gmail.com>
2017-08-21 20:33:05 -0500

    Initialize project
    Create extension.js file and add the first few lines of import
    declarations.

---
+++ extension.js
@@ -0,0 +1,11 @@
+const Lang = imports.lang;
+
+const Clutter = imports.gi.Clutter;
+const St = imports.gi.St;
+
+const Main = imports.ui.main;
+const PanelMenu = imports.ui.panelMenu;
+const PopupMenu = imports.ui.popupMenu;
+
+const Gettext = imports.gettext.domain('window-menu');
+const _ = Gettext.gettext;
