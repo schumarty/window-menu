76629cb71b02b46cb4f13479720bcca05cfa7294
Martin Schumacher <schumarty@gmail.com>
2017-08-27 17:39:28 -0500

    Initialise project
    Create basic outline for shell extension. Currently trying to get the
    title of the menu to appear on the top panel and will add things from
    there.

--- extension.js
+++ extension.js
@@ -1,7 +1,14 @@
+/*
+ * First attempt at creating an extension for GNOME, not totally sure what I'm
+ * doin here. Code is mosty coppied over from the 'places-menu' extension which
+ * is included in GNOME Shell.
+ */
+
 const Lang = imports.lang;
 
 const Clutter = imports.gi.Clutter;
 const St = imports.gi.St;
+const Lang = imports.lang;
 
 const Main = imports.ui.main;
 const PanelMenu = imports.ui.panelMenu;
@@ -9,3 +16,47 @@ const PopupMenu = imports.ui.popupMenu;
 
 const Gettext = imports.gettext.domain('window-menu');
 const _ = Gettext.gettext;
+
+const windowMenu = new Lang.Class({
+  Name: 'WindowMenu.WindowMenu',
+  Extends: PanelMenu.Button,
+
+  _init: function() {
+    // I think this calls the constructor of the parent class but I'm not sure
+    this.parent(0.0, _("Windows"));
+
+    let hbox = new St.BoxLayout({ style_class: 'panel-status-menu-box' });
+    let label = new St.Label({
+      text: _("Windows"),
+      y_expand: true,
+      y_align: Clutter.ActorAlign.CENTER
+    });
+    hbox.add_child(label);
+    hbox.add_child(PopupMenu.arrowIcon(St.Side.BOTTOM));
+
+    this.actor.add_actor(hbox);
+  },
+});
+
+function init() {
+  // Nothing to do here right now
+}
+
+// Track object when extension is enabled so we can remove it upon disable
+let _indicator;
+
+function enable() {
+  _indicator = new WindowMenu;
+
+  // Not sure how this interacts with other items with the same pos number
+  let pos = 1;
+  if ('apps-menu' in Main.panel.statusArea) {
+    pos = 2;
+  }
+  Mainpanel.addToStatusArea('window-menu', _indicator, pos, 'left');
+}
+
+function disable() {
+  _indicator.destroy();
+}
+  
