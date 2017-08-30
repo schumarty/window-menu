d51bf59d8f869e835043990acd7ee60ddb33280e
Martin Schumacher <schumarty@gmail.com>
2017-08-29 21:19:00 -0500

    Get it to work... sort of
    There is now a working dropdown menu that let's active windows be
    selected. Get some odd behavior when opening an app focuses an existing
    app rather than creating a new one. Also, using the 'restacked' event
    doesn't update the window title when the title changes without any
    windows opening or closing.

--- extension.js
+++ extension.js
@@ -15,8 +15,33 @@ const PopupMenu = imports.ui.popupMenu;
 const Gettext = imports.gettext.domain('window-menu');
 const _ = Gettext.gettext;
 
-const windowMenu = new Lang.Class({
-  Name: 'WindowMenu.WindowMenu',
+const MenuItem = new Lang.Class({
+  Name: 'MenuItem',
+  Extends: PopupMenu.PopupBaseMenuItem,
+
+  _init: function(window) {
+    this.parent();
+    this._window = window;
+
+    this._label = new St.Label({ text: this._window.title });
+    this.actor.add_child(this._label);
+
+    global.log('[window-menu]: title=' + this._window.title);
+  },
+
+  destroy: function() {
+    this.parent();
+  },
+
+  activate: function(event) {
+    this._window.activate(global.get_current_time());
+
+    this.parent(event);
+  },
+});
+
+const WindowMenu = new Lang.Class({
+  Name: 'WindowMenu',
   Extends: PanelMenu.Button,
 
   _init: function() {
@@ -27,32 +52,42 @@ const windowMenu = new Lang.Class({
     let label = new St.Label({
       text: _("Windows"),
       y_expand: true,
-      y_align: Clutter.ActorAlign.CENTER
+      y_align: Clutter.ActorAlign.CENTER,
     });
     hbox.add_child(label);
     hbox.add_child(PopupMenu.arrowIcon(St.Side.BOTTOM));
 
     this.actor.add_actor(hbox);
 
-    let section = new PopupMenu.PopupMenuSection();
-
-    this.menu.addMenuItem(section);
+    global.screen.connect('restacked', Lang.bind(this, this._updateMenu));
   },
 
   destroy: function() {
     this.parent();
   },
+
+  _updateMenu: function() {
+    global.log('[window-menu]: updating menu');
+
+    this.menu.removeAll();
+
+    let wkspWindows = global.screen.get_active_workspace().list_windows();
+
+    for (let i = 0; i < wkspWindows.length; i++) {
+      this.menu.addMenuItem(new MenuItem(wkspWindows[i]));
+    }
+  },
 });
 
 function init() {
   // Nothing to do here right now
-};
+}
 
 // Track object when extension is enabled so we can remove it upon disable
 let _indicator;
 
 function enable() {
-  _indicator = new windowMenu();
+  _indicator = new WindowMenu();
 
   // Not sure how this interacts with other items with the same pos number
   let pos = 1;
@@ -60,8 +95,8 @@ function enable() {
     pos = 2;
   }
   Main.panel.addToStatusArea('window-menu', _indicator, pos, 'left');
-};
+}
 
 function disable() {
   _indicator.destroy();
-};
+}
